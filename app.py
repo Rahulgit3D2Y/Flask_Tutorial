43fcd5086c51da3b025b4802587c54f47d6f1aed
rahul chaudhry <109753648+Rahulgit3D2Y@users.noreply.github.com>
2024-08-25 02:28:24 +0000

    added html css

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
+jobs=[{
+  "id":1,
+  'title':"Data Analyst",
+  'location':"Bengaluru, India",
+  'salary':"Rs. 10,00,000"
+},{
+  "id":2,
+  'title':"Data Scientist",
+  'location':"Delhi, India",
+  'salary':"Rs. 15,00,000"
+},{
+  "id":3,
+  'title':"Frontend Engineer",
+  'location':"Remote",
+  'salary':"Rs. 12,00,000"
+}]
+
 @app.route("/")
 def hello():
-    return "Hello World"
+    return render_template("home.html",jobs=jobs)
 
 if __name__=="__main__" :
   app.run(host= "0.0.0.0",debug=True)
